5fdf2cc4ced08350af0e50763d5515ce45fcdf9b
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-19 11:10:01 +0800

    Just installed framer motion and added it to each corresponding pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { Card } from '../components'
 import { Cards } from '../data'
 
 const Home = () => {
   return (
-    <div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <h1 className='bg-indigo-500 font-bold text-2xl p-12 text-white uppercase'>This is a test.</h1>
       <div className='grid lg:grid-cols-3 grid-cols-2'>
         {Cards.map((card, index) => (
@@ -17,7 +23,7 @@ const Home = () => {
           />
         ))}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
