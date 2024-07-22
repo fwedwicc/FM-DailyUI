e56f0081b33fa2b89f2ed99e1c214673aa5b4443
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-22 16:10:32 +0800

    Finished one section and also refactored some components in day03

--- src/pages/Day03.jsx
+++ src/pages/Day03.jsx
@@ -6,6 +6,43 @@ import { motion } from 'framer-motion'
 
 const Day03 = () => {
   useLenisScroll();
+
+  const FeatureCard = ({ desc, title, svg, styles }) => {
+    return (
+      <div className={`space-y-4 ${styles}`}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-green-500">
+          <path strokeLinecap="round" strokeLinejoin="round" d={svg} />
+        </svg>
+        <h1 className="text-xl font-medium text-neutral-300 lg:text-2xl">
+          {title}
+        </h1>
+        <p className="leading-relaxed text-neutral-400">
+          {desc}
+        </p>
+      </div>
+    )
+  }
+
+  const CategoryCard = ({ title, totalJobs }) => {
+    return (
+      <div className='group flex justify-between items-center p-8 rounded-lg bg-neutral-800 border border-neutral-700/40 hover:bg-green-800 hover:border-green-600/40 cursor-pointer transition duration-300 ease-in-out'>
+        <div className='space-y-3'>
+          <h1 className="text-lg font-medium text-neutral-300 lg:text-xl">
+            {title}
+          </h1>
+          <p className="leading-relaxed text-neutral-400 group-hover:text-neutral-300 transition duration-300 ease-in-out">
+            {totalJobs} Jobs
+          </p>
+        </div>
+        <button className='rounded-full p-3 bg-green-700 opacity-0 group-hover:opacity-100 transition duration-300'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-white">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+          </svg>
+        </button>
+      </div>
+    )
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -45,7 +82,7 @@ const Day03 = () => {
               />
             </div>
           </nav>
-          <section className='h-[85vh] flex flex-col gap-5 justify-center items-center lg:px-24 px-12'>
+          <section className='h-[85vh] flex flex-col gap-9 justify-center items-center lg:px-24 px-12'>
             <span className='text-green-500 text-xs uppercase'>Find job in your town</span>
             <div className='text-center space-y-2'>
               <h1 className="text-3xl font-bold text-neutral-200 sm:text-4xl md:text-5xl">
@@ -57,7 +94,7 @@ const Day03 = () => {
               </p>
             </div>
             <div className='text-center'>
-              <div className='flex border border-neutral-700 bg-neutral-900 rounded-[0.8rem] p-2 w-full max-w-xl'>
+              <div className='flex border border-neutral-700 bg-neutral-900 rounded-[0.8rem] p-2 w-full max-w-xl mb-2.5'>
                 <InputField
                   styles={'me-2'}
                   id="job"
@@ -92,43 +129,71 @@ const Day03 = () => {
             </div>
           </section>
         </main>
-        <section className='flex justify-center items-center lg:px-24 px-12 py-6'>
+        <section className='flex justify-center items-center lg:px-24 px-12 pt-6 pb-20'>
           <div className='grid lg:grid-cols-3 grid-cols-2 lg:gap-16 gap-8'>
-            <div className='space-y-4'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-green-500">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-              </svg>
-              <h1 className="text-xl font-medium text-neutral-300 lg:text-2xl">
-                Lorem Ipsum
-              </h1>
+            <FeatureCard
+              title='Lorem Ipsum'
+              desc='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+                quibusdam aperiam voluptatum.'
+              svg='M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z'
+            />
+            <FeatureCard
+              title='Lorem Ipsum'
+              desc='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+                quibusdam aperiam voluptatum.'
+              svg='M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z'
+            />
+            <FeatureCard
+              styles='lg:col-span-1 col-span-2'
+              title='Lorem Ipsum'
+              desc='Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+                quibusdam aperiam voluptatum.'
+              svg='M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z'
+            />
+          </div>
+        </section>
+        <section className='flex bg-neutral-900/40 justify-center items-center lg:px-24 px-12 py-16'>
+          <div className='flex flex-col gap-6 w-full'>
+            <h1 className="text-xl font-medium text-neutral-300 lg:text-2xl">
+              Jobs by category
+            </h1>
+            <div className='flex justify-between items-end'>
               <p className="leading-relaxed text-neutral-400">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. <br /> Eligendi nam dolorum aliquam,
                 quibusdam aperiam voluptatum.
               </p>
+              <a href="#" className='flex items-center gap-1 text-green-500 font-semibold'>
+                Explore all catergories
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+              </a>
             </div>
-            <div className='space-y-4'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-green-500">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-              </svg>
-              <h1 className="text-xl font-medium text-neutral-300 lg:text-2xl">
-                Lorem Ipsum
-              </h1>
-              <p className="leading-relaxed text-neutral-400">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                quibusdam aperiam voluptatum.
-              </p>
-            </div>
-            <div className='lg:col-span-1 col-span-2 space-y-4'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-green-500">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-              </svg>
-              <h1 className="text-xl font-medium text-neutral-300 lg:text-2xl">
-                Lorem Ipsum
-              </h1>
-              <p className="leading-relaxed text-neutral-400">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                quibusdam aperiam voluptatum.
-              </p>
+            <div className='grid grid-cols-3 gap-6'>
+              <CategoryCard
+                title='Design'
+                totalJobs='47'
+              />
+              <CategoryCard
+                title='Marketing'
+                totalJobs='51'
+              />
+              <CategoryCard
+                title='Engineering'
+                totalJobs='89'
+              />
+              <CategoryCard
+                title='Management'
+                totalJobs='16'
+              />
+              <CategoryCard
+                title='Finance'
+                totalJobs='23'
+              />
+              <CategoryCard
+                title='Customer Support'
+                totalJobs='34'
+              />
             </div>
           </div>
         </section>
