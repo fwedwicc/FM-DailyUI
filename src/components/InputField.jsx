0a1c67ce4826756f6a0b34ff84526c149142982c
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-22 12:44:28 +0800

    Modified existing components for day03 challenge

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InputField = ({ id, type, label, placeholder, required }) => {
+const InputField = ({ id, type, label, placeholder, required, styles }) => {
   return (
     <>
       <label htmlFor={id} className="block text-sm font-medium text-neutral-300 mb-1.5">
@@ -9,7 +9,7 @@ const InputField = ({ id, type, label, placeholder, required }) => {
       <input
         id={id}
         type={type}
-        className="block rounded-md w-full bg-neutral-700/20 border border-neutral-600/60 transition duration-300 ease-in-out focus:ring-1 focus:ring-neutral-400 focus:border-neutral-400 text-sm py-2.5 px-3 text-neutral-200 placeholder:text-neutral-400/70"
+        className={`${styles} block rounded-md w-full bg-neutral-700/20 border border-neutral-600/60 transition duration-300 ease-in-out focus:ring-1 focus:ring-neutral-400 focus:border-neutral-400 text-sm py-2.5 px-3 text-neutral-200 placeholder:text-neutral-400/70`}
         placeholder={placeholder}
         required={required}
       />
--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Select = ({ id, label, options = [], required = false }) => {
+const Select = ({ id, label, options = [], required = false, styles }) => {
   return (
     <>
       <label htmlFor={id} className="block text-sm font-medium text-neutral-300 mb-1.5">
@@ -9,7 +9,7 @@ const Select = ({ id, label, options = [], required = false }) => {
       <select
         name={id}
         id={id}
-        className="block rounded-md w-full bg-neutral-800 border border-neutral-600/60 transition duration-300 ease-in-out focus:ring-1 focus:ring-neutral-400 focus:border-neutral-400 text-sm py-2.5 px-3 text-neutral-300"
+        className={`${styles} block rounded-md w-full bg-neutral-800 border border-neutral-600/60 transition duration-300 ease-in-out focus:ring-1 focus:ring-neutral-400 focus:border-neutral-400 text-sm py-2.5 px-3 text-neutral-300`}
         required={required}
       >
         {options.length > 0 ? (
