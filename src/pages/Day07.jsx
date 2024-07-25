51895fe2e0f0e066c1be06d60bb04e11cfe1ac34
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-25 19:52:28 +0800

    Just finished one new section in day07 challenge

--- src/pages/Day07.jsx
+++ src/pages/Day07.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { fmUILogo } from '../assets'
-import { InputField, Checkbox, Button } from '../components'
+import { InputField, Checkbox, Button, Toggle } from '../components'
 import useLenisScroll from '../hooks/useLenisScroll'
 import { motion } from 'framer-motion'
 
@@ -64,6 +64,38 @@ const Day07 = () => {
     'https://avatars.githubusercontent.com/u/98326799?v=4',
     'https://avatars.githubusercontent.com/u/92199408?v=4',
     'https://avatars.githubusercontent.com/u/823537?v=4',
+  ]
+
+  const ThemeIcon = [
+    'https://github.githubassets.com/assets/light_disc-f013f2cb4228.svg',
+    'https://github.githubassets.com/assets/light_high_contrast_disc-f74abfa8e811.svg',
+    'https://github.githubassets.com/assets/light_colorblind_disc-307fec254fd3.svg',
+    'https://github.githubassets.com/assets/light_tritanopia_disc-bac905a50c2c.svg',
+    'https://github.githubassets.com/assets/dark_disc-2bd434b8617d.svg',
+    'https://github.githubassets.com/assets/dark_high_contrast_disc-3820ceab953a.svg',
+    'https://github.githubassets.com/assets/dark_colorblind_disc-cf4710564c71.svg',
+    'https://github.githubassets.com/assets/dark_tritanopia_disc-606f4df188f0.svg',
+    'https://github.githubassets.com/assets/dark_dimmed_disc-ee3bad70351b.svg',
+  ]
+
+  const ThemeContent = [
+    {
+      "icon": "M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z",
+      "theme": "Day theme",
+      "desc": "This theme will be active when your system is set to “light mode”.",
+      "thumbnail": "https://github.githubassets.com/assets/light_preview-0fd4f11e117f.svg",
+      "variant": "Light default",
+      "index": 0
+    },
+    {
+      "icon": "M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z",
+      "theme": "Night theme",
+      "desc": "This theme will be active when your system is set to “dark mode",
+      "thumbnail": "https://github.githubassets.com/assets/dark_dimmed_preview-82b04acf17cc.svg",
+      "variant": "Dark dimmed",
+      "index": 8
+    },
+
   ]
 
   return (
@@ -212,6 +244,64 @@ const Day07 = () => {
                       />
                       <span className="flex w-3 h-3 ms-2 bg-green-500 rounded-full"></span>
                     </div>
+                  </div>
+                }
+              />
+              {/* Interface Theme */}
+              <SectionContainer
+                child1={
+                  <div className='space-y-2'>
+                    <h1 className="font-medium text-neutral-200 text-lg md:text-xl">
+                      Interface Theme
+                    </h1>
+                    <p className="leading-relaxed text-neutral-400 text-sm">
+                      Select a UI theme or sync with your system.
+                    </p>
+                    <Toggle>
+                      <span className="ms-3 text-sm font-medium text-neutral-300">
+                        Sync with system
+                      </span>
+                    </Toggle>
+                  </div>
+                }
+                child2={
+                  <div className='flex md:flex-nowrap flex-wrap items-start gap-4'>
+                    {ThemeContent.map((content, index) => {
+                      return (
+                        <div className="inline-flex w-full md:max-w-sm flex-col items-start rounded-lg border border-neutral-700" key={index}>
+                          <div className="flex items-center border-b border-neutral-700 w-full py-4 px-5">
+                            <div className='inline-flex gap-2 items-center text-neutral-300'>
+                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d={content.icon} />
+                              </svg>
+                              <span className='font-medium'>{content.theme}</span>
+                            </div>
+                          </div>
+                          <div className='w-full p-4 space-y-5'>
+                            <span className="text-xs font-thin text-neutral-400">
+                              {content.desc}
+                            </span>
+                            <img src={content.thumbnail} alt="Light Mode Thumbnail" className='rounded-md w-full h-auto' />
+                            <p className="text-xs font-thin text-neutral-300 mt-1">
+                              {content.variant}
+                            </p>
+                            <div className='flex flex-wrap items-center gap-3'>
+                              {ThemeIcon.map((icon, index) => (
+                                <div key={index}>
+                                  <img
+                                    className={`size-9 border border-neutral-500 rounded-full ${index === content.index ? 'border-[3px] border-neutral-700 ring-[2px] ring-green-500' : ''
+                                      }`}
+                                    src={icon}
+                                    alt="Theme Icon"
+                                  />
+                                </div>
+                              ))}
+                            </div>
+                          </div>
+                        </div>
+                      )
+                    })}
+
                   </div>
                 }
               />
