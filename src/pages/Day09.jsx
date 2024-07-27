0fe9ba87c33c9462428c2546b803bbbe20fb98e0
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-27 12:18:21 +0800

    Added initial layout for day09 challenge

--- src/pages/Day09.jsx
+++ src/pages/Day09.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { fmUILogo } from '../assets'
 import { InputField, Checkbox, Button } from '../components'
 import useLenisScroll from '../hooks/useLenisScroll'
-import useScrollToTop from '../hooks/useScrollToTop';
+import useScrollToTop from '../hooks/useScrollToTop'
 import { motion } from 'framer-motion'
 
 const Day09 = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   useLenisScroll();
   useScrollToTop();
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <motion.div
@@ -16,11 +22,27 @@ const Day09 = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <section className="bg-neutral-800">
-        <h1>sss</h1>
+      <section className="bg-neutral-800 flex">
+        <aside
+          className={`fixed h-screen bg-neutral-700 w-64 transition-transform transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+            } lg:translate-x-0`}
+        >
+          {/* Sidebar */}
+        </aside>
+        <main className="flex-1 ml-0 lg:ml-64 lg:mt-0 mt-[4.5rem] bg-neutral-800">
+          {/* Sidebar Toggle Button for Medium Screens */}
+          <button
+            className="lg:hidden fixed top-4 left-4 p-2 bg-neutral-700 text-white rounded-md z-50"
+            onClick={toggleSidebar}
+          >
+            {isSidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}
+          </button>
+          {/* Main Content */}
+          <h1>sss</h1>
+        </main>
       </section>
     </motion.div>
-  )
-}
+  );
+};
 
-export default Day09
+export default Day09;
