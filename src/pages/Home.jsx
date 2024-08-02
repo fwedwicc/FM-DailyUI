fdcbdb5ec0117e9517bc3f2f4af35d576115bf29
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-02 13:23:35 +0800

    Added footer component for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
 
   const Hero = () => {
     return (
-      <main className="relative lg:h-screen h-auto grid lg:grid-cols-2 grid-cols-1">
+      <main className="relative lg:h-screen h-auto grid lg:grid-cols-2 grid-cols-1" id='home'>
         <img src={glowEllipse} alt="Glow Ellipse" className="absolute inset-0 z-0" />
         <div className="rounded-2xl p-[1px] bg-border-gradient z-10 md:mt-10 mt-7 md:ml-10 ml-7">
           <div className='relative rounded-2xl h-full md:p-16 p-7 bg-neutral-900'>
@@ -70,6 +70,59 @@ const Home = () => {
     )
   }
 
+  const Footer = () => {
+    return (
+      <footer className='py-8 px-16 flex md:flex-row gap-6 flex-col items-center justify-between'>
+        <div className='flex md:flex-row flex-col items-center gap-5'>
+          <a href="#home">
+            <img src={FMLogo} alt="FM Logo" className='w-8 h-auto' />
+          </a>
+          <p className="inline-flex items-center text-xs leading-relaxed text-green-50/80">
+            Crafted with
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-3 mx-[5px]">
+              <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+            </svg>
+            by
+            <a href='#' className='text-green-500 ms-[5px]'>Frederick Moreno</a>
+          </p></div>
+        <ul className='flex items-center md:gap-2 gap-1'>
+          {[
+            {
+              link: "#",
+              icon: "M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z"
+            },
+            {
+              link: "#",
+              icon: "M12.51 8.796v1.697a3.738 3.738 0 0 1 3.288-1.684c3.455 0 4.202 2.16 4.202 4.97V19.5h-3.2v-5.072c0-1.21-.244-2.766-2.128-2.766-1.827 0-2.139 1.317-2.139 2.676V19.5h-3.19V8.796h3.168ZM7.2 6.106a1.61 1.61 0 0 1-.988 1.483 1.595 1.595 0 0 1-1.743-.348A1.607 1.607 0 0 1 5.6 4.5a1.601 1.601 0 0 1 1.6 1.606Z",
+              extra: "M7.2 8.809H4V19.5h3.2V8.809Z"
+            },
+            {
+              link: "#",
+              icon: "M12 2a10 10 0 1 0 10 10A10.009 10.009 0 0 0 12 2Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.093 20.093 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM10 3.707a8.82 8.82 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.755 45.755 0 0 0 10 3.707Zm-6.358 6.555a8.57 8.57 0 0 1 4.73-5.981 53.99 53.99 0 0 1 3.168 4.941 32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.641 31.641 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM12 20.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 15.113 13a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
+            },
+            {
+              link: "#",
+              icon: "M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z"
+            },
+            {
+              link: "#",
+              icon: "M3 8a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v8a5 5 0 0 1-5 5H8a5 5 0 0 1-5-5V8Zm5-3a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h8a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H8Zm7.597 2.214a1 1 0 0 1 1-1h.01a1 1 0 1 1 0 2h-.01a1 1 0 0 1-1-1ZM12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6Zm-5 3a5 5 0 1 1 10 0 5 5 0 0 1-10 0Z"
+            },
+          ].map((social, index) => (
+            <li key={index}>
+              <a href={social.link} className='inline-block p-2 hover:bg-neutral-700/50 transition duration-300 ease-in-out rounded-md'>
+                <svg className="size-5 text-neutral-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                  <path fill-rule="evenodd" d={social.icon} clip-rule="evenodd" />
+                  <path d={social.extra} />
+                </svg>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    )
+  }
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -91,14 +144,14 @@ const Home = () => {
             </svg>
           </div>
           <p className="text-sm leading-relaxed text-center text-green-50/80">
-            This section showcases my Daily UI challenge submissions, each meticulously crafted using React and Tailwind CSS. Every design and implementation is originally created by <span className='text-green-500'>Frederick Moreno</span>, reflecting my dedication to improving my skills in front-end development and UI/UX design.
+            This section showcases my Daily UI challenge submissions, each meticulously crafted using React and Tailwind CSS. Every design and implementation is crafted by <span className='text-green-500'>Frederick Moreno</span>, reflecting my dedication to improving my skills in front-end development and UI/UX design.
           </p>
           <p className="text-sm leading-relaxed text-center text-green-50/80">
             <strong>Disclaimer:</strong> All works presented here are my original creations. Special thanks to <a href="https://www.dailyui.co/" className='underline'>Daily UI</a> for providing the prompts that inspired these designs. Additionally, some assets and resources used in these projects are credited to their <strong>respective creators</strong> found on the internet.
           </p>
         </div>
       </div>
-      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 lg:px-20 md:px-12 px-6 py-12'>
+      <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 lg:px-16 md:px-12 px-6 py-12'>
         {Cards.map((card, index) => (
           <Card
             key={index}
@@ -110,6 +163,18 @@ const Home = () => {
           />
         ))}
       </div>
+      <div className='md:py-12 py-7 px-12 flex justify-center items-center'>
+        <div className='w-full max-w-lg space-y-7'>
+          <p className="text-sm leading-relaxed text-center text-green-50/80">
+            So you've reached this far! I don't think many viewers did either. Please consider giving this <a href="https://github.com/fwedwicc/FM-DailyUI" className='underline text-green-500'>GitHub repository</a> a star. Your support means a lot. <span className='inline-flex items-center gap-1'>
+              Thank you so much for your appreciation!
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-3">
+                <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+              </svg></span>
+          </p>
+        </div>
+      </div>
+      <Footer />
     </motion.div>
   )
 }
