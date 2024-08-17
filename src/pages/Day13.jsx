36514ee3a8864d0249965d68f83f4d52ad82f6eb
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-17 12:54:26 +0800

    Added initial layout for day13 challenge

--- src/pages/Day13.jsx
+++ src/pages/Day13.jsx
@@ -17,7 +17,34 @@ const Day13 = () => {
       transition={{ duration: 0.5 }}
     >
       <section className="bg-neutral-800">
-        <h1>hello</h1>
+        <div className="flex lg:flex-row flex-col h-screen">
+          {/* Nav buttons in mobile screens */}
+          <div className='flex justify-between items-center lg:hidden'>
+            {/* Button for left bar */}
+            <button
+              className="lg:hidden py-2 size-9 flex justify-center items-center bg-neutral-600/50 backdrop-blur-sm hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
+              </svg>
+            </button>
+            {/* Button for Right bar */}
+            <button
+              className="lg:hidden py-2 size-9 flex justify-center items-center bg-neutral-600/50 backdrop-blur-sm hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out"
+            />
+          </div>
+          {/* Left bar */}
+          <div className="lg:block hidden h-full border text-wrap p-6 w-full max-w-[20rem]">
+            <p className='text-neutral-300'>Left Content</p>
+          </div>
+          <div className="flex-grow h-full border text-wrap lg:mt-0 mt-12">
+            <p className='text-neutral-300'>Middle Content</p>
+          </div>
+          {/* Right bar */}
+          <div className="lg:block hidden h-full border text-wrap p-6 w-full max-w-[20rem]">
+            <p className='text-neutral-300'>Right Content</p>
+          </div>
+        </div>
       </section>
     </motion.div>
   )
