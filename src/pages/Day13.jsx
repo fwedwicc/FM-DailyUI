f4a0055a72653568e683584596d6dd10c30a3161
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-06 15:29:10 +0800

    "Updated top container with avatar, username, member count, and buttons"

--- src/pages/Day13.jsx
+++ src/pages/Day13.jsx
@@ -160,11 +160,46 @@ const Day13 = () => {
           <div className="flex-grow h-full text-wrap lg:pl-[24.6rem] p-3">
             <div className='relative h-full bg-neutral-800 rounded-t-2xl rounded-b-[1.7rem]'>
               {/* Top Container */}
-              <div className='absolute top-0 border rounded-t-2xl w-full'>
-                <p className='text-neutral-300'>Top Container</p>
+              <div className='flex justify-between items-center absolute top-0 border rounded-t-2xl w-full p-4'>
+                <div className='flex items-center gap-5'>
+                  <img
+                    className="size-10 border-2 ring-2 ring-green-500 border-neutral-700 rounded-full cursor-pointer"
+                    src="https://avatars.githubusercontent.com/u/84505567?v=4"
+                    alt="Bot Avatar"
+                  />
+                  <div>
+                    <h1 className="leading-none font-normal text-neutral-200 text-md md:text-xl">
+                      Tech-Commr
+                    </h1>
+                    <span className="flex items-center gap-3 leading-none text-neutral-300 text-sm">
+                      3 members
+                      <span>
+                        <span className='inline-flex size-2 rounded-full bg-green-500 me-2'></span>
+                        1 online
+                      </span>
+                    </span>
+                  </div>
+                </div>
+                <div className='flex gap-1.5'>
+                  <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                      <path d="M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z" />
+                    </svg>
+                  </button>
+                  <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                      <path fillRule="evenodd" d="M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                  <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                      <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
               </div>
               {/* Chat Contents */}
-              <div className='py-12 h-full'>hello</div>
+              <div className='py-24 h-full'>hello</div>
               {/* Bottom Container */}
               <div className='absolute bottom-0 border w-full p-3'>
                 <div className='w-full relative'>
