690b71dbb10177f658bcbbc432f6446a06724423
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-07 16:05:59 +0800

    "Removed LianMessage component, updated right bar layout and added channel details, audio/video/mute/leave buttons, and pinned messages/media/files links sections"

--- src/pages/Day13.jsx
+++ src/pages/Day13.jsx
@@ -270,7 +270,6 @@ const Day13 = () => {
               </div>
               {/* Chat Contents */}
               <div className='py-24 h-full overflow-auto z-30 flex flex-col justify-end items-end px-6 gap-y-2.5'>
-                <LianMessage />
                 <KielMessage />
                 <FredMessage />
                 Todeyyyyyy
@@ -323,8 +322,97 @@ const Day13 = () => {
             </div>
           </div>
           {/* Right bar */}
-          <div className="lg:block hidden h-full border text-wrap p-6 w-full md:min-w-80 lg:min-w-96 md:max-w-80 lg:max-w-96">
-            <p className='text-neutral-300'>Right Content</p>
+          <div className="lg:block hidden h-full border text-wrap p-3 w-full md:min-w-80 lg:min-w-96 md:max-w-80 lg:max-w-96">
+            {/*  */}
+            <div className='flex justify-between items-center border'>
+              <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                  <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z" clipRule="evenodd" />
+                </svg>
+              </button>
+              <h1 className="leading-none font-normal text-neutral-200 text-md md:text-xl">
+                Channel Details
+              </h1>
+              <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
+                </svg>
+              </button>
+            </div>
+            {/*  */}
+            <div className='flex flex-col items-center gap-3 mt-3'>
+              <img
+                className="size-20 border-2 ring-2 ring-green-500 border-neutral-700 rounded-full cursor-pointer"
+                src="https://avatars.githubusercontent.com/u/84505567?v=4"
+                alt="Bot Avatar"
+              />
+              <div className='space-y-1'>
+                <h1 className="text-center leading-none font-normal text-neutral-200 text-xl md:text-2xl">
+                  Tech-Commr
+                </h1>
+                <span className="flex items-center gap-3 leading-none text-neutral-300 text-sm">
+                  3 members
+                  <span>
+                    <span className='inline-flex size-2 rounded-full bg-green-500 me-2'></span>
+                    1 online
+                  </span>
+                </span>
+              </div>
+            </div>
+            {/*  */}
+            <div className='flex gap-3 w-full border mt-4'>
+              {[
+                {
+                  label: "Audio", path: "M1.5 4.5a3 3 0 0 1 3-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 0 1-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 0 0 6.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 0 1 1.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 0 1-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5Z"
+                },
+                {
+                  label: "Video", path: "M4.5 4.5a3 3 0 0 0-3 3v9a3 3 0 0 0 3 3h8.25a3 3 0 0 0 3-3v-9a3 3 0 0 0-3-3H4.5ZM19.94 18.75l-2.69-2.69V7.94l2.69-2.69c.944-.945 2.56-.276 2.56 1.06v11.38c0 1.336-1.616 2.005-2.56 1.06Z"
+                },
+                {
+                  label: "Mute", path: "M5.25 9a6.75 6.75 0 0 1 13.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 0 1-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 1 1-7.48 0 24.585 24.585 0 0 1-4.831-1.244.75.75 0 0 1-.298-1.205A8.217 8.217 0 0 0 5.25 9.75V9Zm4.502 8.9a2.25 2.25 0 1 0 4.496 0 25.057 25.057 0 0 1-4.496 0Z"
+                },
+                {
+                  label: "Leave", path: "M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z"
+                }
+              ].map((action, index) => (
+                <button className='px-4 py-3 flex gap-2 flex-col flex-grow justify-center items-center bg-neutral-700/40 hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out' key={index}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                    <path fillRule="evenodd" d={action.path} clipRule="evenodd" />
+                  </svg>
+                  <p className="text-neutral-300 text-xs">
+                    {action.label}
+                  </p>
+                </button>
+              ))}
+            </div>
+            {/*  */}
+            <div className='flex flex-col w-full gap-2.5 mt-2.5 border'>
+              {[
+                {
+                  label: "Media, files & links", path: "M18.97 3.659a2.25 2.25 0 0 0-3.182 0l-10.94 10.94a3.75 3.75 0 1 0 5.304 5.303l7.693-7.693a.75.75 0 0 1 1.06 1.06l-7.693 7.693a5.25 5.25 0 1 1-7.424-7.424l10.939-10.94a3.75 3.75 0 1 1 5.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 0 1 5.91 15.66l7.81-7.81a.75.75 0 0 1 1.061 1.06l-7.81 7.81a.75.75 0 0 0 1.054 1.068L18.97 6.84a2.25 2.25 0 0 0 0-3.182Z"
+                },
+                {
+                  label: "Pinned messages", path: "m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                },
+                {
+                  label: "Channel settings", path: "M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
+                }
+              ].map((action, index) => (
+                <button className='px-4 py-3 flex gap-2 flex-grow justify-between items-center bg-neutral-700/40 hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out' key={index}>
+                  <div className='flex items-center gap-3'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                      <path fillRule="evenodd" d={action.path} clipRule="evenodd" />
+                    </svg>
+                    <p className="text-neutral-300 text-sm">
+                      {action.label}
+                    </p>
+                  </div>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                    <path fillRule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clipRule="evenodd" />
+                  </svg>
+                </button>
+              ))}
+            </div>
           </div>
         </div>
       </section>
