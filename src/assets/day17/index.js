60b7ad761273f4b80d294256fe4caf466e2f4821
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-10-13 13:08:40 +0800

    Added image assets for some components

---
+++ src/assets/day17/index.js
@@ -0,0 +1,15 @@
+import ProductImage from './product.png'
+import MasterCard from './mastercard.svg'
+import FacebookIcon from './facebook.svg'
+import InstagramIcon from './instagram.svg'
+import TikTokIcon from './tiktok.svg'
+import QRCode from './QR-code.png'
+
+export {
+  ProductImage,
+  MasterCard,
+  FacebookIcon,
+  InstagramIcon,
+  TikTokIcon,
+  QRCode
+}
