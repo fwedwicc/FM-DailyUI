c4e7afb1cef373960ec1bb2a1e87b9bb1b416a61
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-11-25 20:15:13 +0800

    "Updated Day20 component: replaced image src, added drawer component with indicator, content, and buttons, and updated styles."

--- src/pages/Day20.jsx
+++ src/pages/Day20.jsx
@@ -62,11 +62,49 @@ const Day20 = () => {
               <div className='h-full relative border'>
                 {/* Map */}
                 <div className='relative w-full h-72'>
-                  <img src="https://placehold.co/30x30" alt="" className='h-full w-full object-cover rounded-t-3xl' />
+                  <img src="" alt="" className='border h-full w-full object-cover rounded-t-3xl' />
                 </div>
-                {/* Main Content */}
-                <div className='absolute bottom-0 border border-red-500 bg-green-500 w-full h-80 rounded-3xl'>
-                  <div className='absolute top-1.5 h-1.5 w-12 rounded-full bg-red-500 transform left-1/2 -translate-x-1/2'></div>
+                {/* Drawer */}
+                <div className='absolute bottom-0 border bg-neutral-800 w-full h-80 rounded-3xl'>
+                  {/* Drawer indicator */}
+                  <div className='absolute top-1.5 h-1.5 w-12 rounded-full border transform left-1/2 -translate-x-1/2'></div>
+                  {/* Content */}
+                  <div className='mt-5 px-2'>
+                    <div className='border flex items-center justify-between px-2'>
+                      <div className='flex items-center gap-2.5'>
+                        <div className='flex flex-col items-center justify-center h-10 py-1'>
+                          <div className='shrink-0 size-2 rounded-full bg-green-500'></div>
+                          <div className='w-[2px] h-full bg-green-500'></div>
+                          <div className='shrink-0 size-2 rounded-full bg-neutral-500'></div>
+                        </div>
+                        <div className='space-y-2'>
+                          <p className='text-neutral-300 text-xs w-full max-w-xs truncate'>Bagong Silang, Caloocan City</p>
+                          <p className='text-neutral-300 text-xs truncate'>Quezon City University</p>
+                        </div>
+                      </div>
+                      <button className='size-8 flex justify-center items-center text-neutral-300 bg-neutral-600/50 hover:bg-neutral-200/20 rounded-full focus:ring-2 focus:ring-neutral-500 z-50 transition duration-300 ease-in-out'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.046-2.453-.138-3.662a4.006 4.006 0 0 0-3.7-3.7 48.678 48.678 0 0 0-7.324 0 4.006 4.006 0 0 0-3.7 3.7c-.017.22-.032.441-.046.662M19.5 12l3-3m-3 3-3-3m-12 3c0 1.232.046 2.453.138 3.662a4.006 4.006 0 0 0 3.7 3.7 48.656 48.656 0 0 0 7.324 0 4.006 4.006 0 0 0 3.7-3.7c.017-.22.032-.441.046-.662M4.5 12l3 3m-3-3-3 3" />
+                        </svg>
+                      </button>
+                    </div>
+                    <div className='flex items-center gap-3 mt-2'>
+                      <button className='flex justify-center items-center gap-1.5 text-neutral-300 bg-neutral-600/50 hover:bg-neutral-200/20 rounded-md px-2 py-1 focus:ring-2 focus:ring-neutral-500 transition duration-300 ease-in-out text-xs'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M15.59 14.37a6 6 0 0 1-5.84 7.38v-4.8m5.84-2.58a14.98 14.98 0 0 0 6.16-12.12A14.98 14.98 0 0 0 9.631 8.41m5.96 5.96a14.926 14.926 0 0 1-5.841 2.58m-.119-8.54a6 6 0 0 0-7.381 5.84h4.8m2.581-5.84a14.927 14.927 0 0 0-2.58 5.84m2.699 2.7c-.103.021-.207.041-.311.06a15.09 15.09 0 0 1-2.448-2.448 14.9 14.9 0 0 1 .06-.312m-2.24 2.39a4.493 4.493 0 0 0-1.757 4.306 4.493 4.493 0 0 0 4.306-1.758M16.5 9a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+                        </svg>
+                        All modes selected
+                      </button>
+                      <button className='flex justify-center items-center gap-1.5 text-neutral-300 bg-neutral-600/50 hover:bg-neutral-200/20 rounded-md px-2 py-1 focus:ring-2 focus:ring-neutral-500 transition duration-300 ease-in-out text-xs'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-3">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                        </svg>
+                        Leaving now
+                      </button>
+                    </div>
+                    {/*  */}
+                    <div></div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -77,11 +115,11 @@ const Day20 = () => {
               <div className='h-full relative border'>
                 {/* Map */}
                 <div className='relative w-full h-72'>
-                  <img src="https://placehold.co/30x30" alt="" className='h-full w-full object-cover rounded-t-3xl' />
+                  <img src="" alt="" className='border h-full w-full object-cover rounded-t-3xl' />
                 </div>
                 {/* Main Content */}
-                <div className='absolute bottom-0 border border-red-500 bg-green-500 w-full h-80 rounded-3xl'>
-                  <div className='absolute top-1.5 h-1.5 w-12 rounded-full bg-red-500 transform left-1/2 -translate-x-1/2'></div>
+                <div className='absolute bottom-0 border bg-neutral-800 w-full h-80 rounded-3xl'>
+                  <div className='absolute top-1.5 h-1.5 w-12 rounded-full border transform left-1/2 -translate-x-1/2'></div>
                 </div>
               </div>
             </div>
