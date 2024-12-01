4a4c36ce6fc2436bf8225ea5ac4fe731a7d1b208
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-12-01 13:44:58 +0800

    "Removed useNavigate import, updated pagination button styles, and modified footer text and links"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { FMLogo } from '../assets'
 import { motion } from 'framer-motion'
 import useLenisScroll from '../hooks/useLenisScroll'
@@ -17,8 +17,6 @@ const Home = () => {
   const cardsPerPage = 10;
 
   const [isPaginationTriggered, setIsPaginationTriggered] = useState(false);
-
-  // Get current page from URL, default to 1
   const currentPage = parseInt(searchParams.get('page') || '1', 10);
 
   // Cards to display
@@ -229,7 +227,7 @@ const Home = () => {
               key={index}
               className={`flex items-center justify-center md:size-9 size-8 md:text-sm text-xs rounded-md ${currentPage === index + 1
                 ? 'bg-green-500 text-white'
-                : 'bg-neutral-700/50 text-neutral-300 hover:bg-neutral-700/50'
+                : 'bg-neutral-700/30 text-neutral-300 hover:bg-neutral-700/60'
                 } transition-all duration-300`}
               onClick={() => handlePageChange(index + 1)}
             >
@@ -255,7 +253,7 @@ const Home = () => {
       <div className='md:py-12 py-7 px-12 flex justify-center items-center'>
         <div className='w-full max-w-lg space-y-7'>
           <p className="text-sm leading-relaxed text-center text-green-50/80">
-            Please consider giving this <a href="https://github.com/fwedwicc/FM-DailyUI" className='underline text-green-500' target='_blank' rel='noopener noreferrer'>GitHub repository</a> a star. Your support means a lot. <span className='inline-flex items-center gap-1'>
+            Your support means the world! If you find this helpful, please consider giving my <a href="https://github.com/fwedwicc/FM-DailyUI" className='underline text-green-500' target='_blank' rel='noopener noreferrer'>GitHub</a> repository a star. Feel free to explore my works on <a href="https://dribbble.com/fwedwic" className='underline text-green-500' target='_blank' rel='noopener noreferrer'>Dribbble</a>. <span className='inline-flex items-center gap-1'>
               Thank you so much for your appreciation!
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-3">
                 <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
