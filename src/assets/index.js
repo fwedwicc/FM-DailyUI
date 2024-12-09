1a72877e97e6b42ca138d673cc043520d70bc8ec
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-12-09 19:13:54 +0800

    Added VrGlass image to assets and updated Day22 and UpcomingChallenge pages to include new illustration and improved layout

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,11 @@
 import fmUILogo from './fmUI-logo.png'
 import FMLogo from './FM-logo.png'
 import UICompilation from './ui-compilation-bg.png'
+import VrGlass from './vr-glass.gif'
 
 export {
   fmUILogo,
   FMLogo,
   UICompilation,
+  VrGlass,
 }
--- src/pages/UpcomingChallenge.jsx
+++ src/pages/UpcomingChallenge.jsx
@@ -17,7 +17,8 @@ const UpcomingChallenge = () => {
     >
       <main className='h-screen flex items-center justify-center'>
         <img src={UICompilation} alt="UI Compliation" className='absolute object-cover h-full w-full' />
-        <div className='z-20 flex flex-col items-center justify-center md:gap-3 gap-1'>
+        <div className='absolute w-full h-full bg-neutral-800/40'></div>
+        <div className='z-20 flex flex-col items-center justify-center md:gap-3 gap-1 transition-all duration-300 ease-in-out'>
           <img src={fmUILogo} alt="FM Logo" className='mb-10 md:w-20 w-12 h-auto' />
           <h1 className='text-neutral-300 md:text-3xl text-2xl font-medium'>Upcoming Challenge</h1>
           <p className="md:text-sm text-xs text-center font-medium text-neutral-400">This challenge has not yet been revealed. <br /> Stay tuned for updates!</p>
