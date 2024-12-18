61d2a8e55399e06ab2ee80c8996c058e6fffecc9
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-12-18 22:07:49 +0800

    Refactored SearchBar and header components in Draft page; improved layout and responsiveness, added icon functionality, and enhanced image presentation.

--- src/pages/Draft.jsx
+++ src/pages/Draft.jsx
@@ -11,7 +11,7 @@ const Draft = () => {
 
   const SearchBar = ({ styles }) => {
     return (
-      <div className={`${styles} flex-col border gap-2 w-full`}>
+      <div className={`${styles} flex-col gap-2 w-full`}>
         {/* Search bar */}
         <div className='flex items-center gap-4'>
           <div className='w-full relative'>
@@ -22,10 +22,18 @@ const Draft = () => {
               className={`w-full rounded-md bg-neutral-700/20 border border-neutral-600/60 py-2.5 px-3 pe-10 text-sm transition duration-300 ease-in-out focus:ring-neutral-400 focus:border-neutral-400 focus:ring-1 text-neutral-200 placeholder:text-neutral-400/70`}
               required={false}
             />
-            <div className="pointer-events-none absolute inset-y-0 end-0 flex items-center pe-3">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5 text-neutral-300">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-              </svg>
+            <div className="absolute inset-y-0 end-0 flex items-center pe-3">
+              {['M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z', 'M9 4.5a.75.75 0 0 1 .721.544l.813 2.846a3.75 3.75 0 0 0 2.576 2.576l2.846.813a.75.75 0 0 1 0 1.442l-2.846.813a3.75 3.75 0 0 0-2.576 2.576l-.813 2.846a.75.75 0 0 1-1.442 0l-.813-2.846a3.75 3.75 0 0 0-2.576-2.576l-2.846-.813a.75.75 0 0 1 0-1.442l2.846-.813A3.75 3.75 0 0 0 7.466 7.89l.813-2.846A.75.75 0 0 1 9 4.5ZM18 1.5a.75.75 0 0 1 .728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 0 1 0 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 0 1-1.456 0l-.258-1.036a2.625 2.625 0 0 0-1.91-1.91l-1.036-.258a.75.75 0 0 1 0-1.456l1.036-.258a2.625 2.625 0 0 0 1.91-1.91l.258-1.036A.75.75 0 0 1 18 1.5ZM16.5 15a.75.75 0 0 1 .712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 0 1 0 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 0 1-1.422 0l-.395-1.183a1.5 1.5 0 0 0-.948-.948l-1.183-.395a.75.75 0 0 1 0-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0 1 16.5 15Z', ['M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z', 'M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z'], 'M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z',].map((item, index) => (
+                <span className={`${index === 0 ? 'border-r border-neutral-600 pr-3' : ''} px-2.5`} key={index}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${index === 1 ? 'text-green-500' : 'text-neutral-300'} cursor-pointer size-5`}>
+                    {Array.isArray(item) ? (
+                      item.map((d, i) => <path key={i} strokeLinecap="round" strokeLinejoin="round" d={d} />)
+                    ) : (
+                      <path fillRule="evenodd" clipRule="evenodd" d={item} />
+                    )}
+                  </svg>
+                </span>
+              ))}
             </div>
           </div>
         </div>
@@ -33,16 +41,20 @@ const Draft = () => {
         <div className='flex items-center justify-between'>
           {/* Tab Component */}
           <div className='flex items-center gap-4 cursor-pointer text-neutral-400 text-sm'>
-            <span>All</span>
-            <span>Images</span>
-            <span>Videos</span>
-            <span>Shopping</span>
-            <span>News</span>
-            <span>More</span>
+            {['All', 'Images', 'Videos', 'Shopping', 'News', 'More'].map((item, index) => (
+              <span className={`${index === 0 ? 'flex items-center gap-1 border-b-2 pr-1 text-green-500 border-green-500' : ''}`} key={index}>
+                {index === 0 ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                    <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
+                  </svg>
+                ) : (null)}
+                {item}
+              </span>
+            ))}
           </div>
           <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+              <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
             </svg>
           </button>
         </div>
@@ -263,8 +275,8 @@ const Draft = () => {
     >
       <main className="bg-neutral-800 space-y-6">
         {/* Header */}
-        <div className='bg-[#2b2b2b] md:p-6 p-4 sticky top-0'>
-          <header className='flex border justify-between items-start'>
+        <div className='bg-[#2b2b2b] md:p-6 md:pb-2 p-4 pb-2 sticky top-0'>
+          <header className='flex justify-between items-start'>
             <div className='flex items-start gap-36 w-full'>
               {/* Logo */}
               <img src={fmUILogo} alt="fmUI Logo" className='w-16 h-auto' />
@@ -272,17 +284,28 @@ const Draft = () => {
               <SearchBar styles='lg:flex hidden lg:max-w-3xl max-w-md' />
             </div>
             {/* Settings */}
-            <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out'>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M12 18.75a6 6 0 0 0 6-6v-1.5m-6 7.5a6 6 0 0 1-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 0 1-3-3V4.5a3 3 0 1 1 6 0v8.25a3 3 0 0 1-3 3Z" />
-              </svg>
-            </button>
+            <div className='flex items-center gap-3'>
+              {[
+                "M10.5 3.798v5.02a3 3 0 0 1-.879 2.121l-2.377 2.377a9.845 9.845 0 0 1 5.091 1.013 8.315 8.315 0 0 0 5.713.636l.285-.071-3.954-3.955a3 3 0 0 1-.879-2.121v-5.02a23.614 23.614 0 0 0-3 0Zm4.5.138a.75.75 0 0 0 .093-1.495A24.837 24.837 0 0 0 12 2.25a25.048 25.048 0 0 0-3.093.191A.75.75 0 0 0 9 3.936v4.882a1.5 1.5 0 0 1-.44 1.06l-6.293 6.294c-1.62 1.621-.903 4.475 1.471 4.88 2.686.46 5.447.698 8.262.698 2.816 0 5.576-.239 8.262-.697 2.373-.406 3.092-3.26 1.47-4.881L15.44 9.879A1.5 1.5 0 0 1 15 8.818V3.936Z", "M11.622 1.602a.75.75 0 0 1 .756 0l2.25 1.313a.75.75 0 0 1-.756 1.295L12 3.118 10.128 4.21a.75.75 0 1 1-.756-1.295l2.25-1.313ZM5.898 5.81a.75.75 0 0 1-.27 1.025l-1.14.665 1.14.665a.75.75 0 1 1-.756 1.295L3.75 8.806v.944a.75.75 0 0 1-1.5 0V7.5a.75.75 0 0 1 .372-.648l2.25-1.312a.75.75 0 0 1 1.026.27Zm12.204 0a.75.75 0 0 1 1.026-.27l2.25 1.312a.75.75 0 0 1 .372.648v2.25a.75.75 0 0 1-1.5 0v-.944l-1.122.654a.75.75 0 1 1-.756-1.295l1.14-.665-1.14-.665a.75.75 0 0 1-.27-1.025Zm-9 5.25a.75.75 0 0 1 1.026-.27L12 11.882l1.872-1.092a.75.75 0 1 1 .756 1.295l-1.878 1.096V15a.75.75 0 0 1-1.5 0v-1.82l-1.878-1.095a.75.75 0 0 1-.27-1.025ZM3 13.5a.75.75 0 0 1 .75.75v1.82l1.878 1.095a.75.75 0 1 1-.756 1.295l-2.25-1.312a.75.75 0 0 1-.372-.648v-2.25A.75.75 0 0 1 3 13.5Zm18 0a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-.372.648l-2.25 1.312a.75.75 0 1 1-.756-1.295l1.878-1.096V14.25a.75.75 0 0 1 .75-.75Zm-9 5.25a.75.75 0 0 1 .75.75v.944l1.122-.654a.75.75 0 1 1 .756 1.295l-2.25 1.313a.75.75 0 0 1-.756 0l-2.25-1.313a.75.75 0 1 1 .756-1.295l1.122.654V19.5a.75.75 0 0 1 .75-.75Z"
+              ].map((button, index) => (
+                <button className='size-9 shrink-0 flex justify-center items-center hover:bg-neutral-700/70 rounded-md focus:ring-2 focus:ring-neutral-700 text-neutral-400 z-50 transition duration-300 ease-in-out' key={index}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+                    <path fillRule="evenodd" d={button} clipRule="evenodd" />
+                  </svg>
+                </button>
+              ))}
+              <img
+                className="shrink-0 size-8 border-2 ring-2 ring-green-500 border-neutral-700 rounded-full cursor-pointer me-5"
+                src="https://avatars.githubusercontent.com/u/823537?v=4"
+                alt="Bot Avatar"
+              />
+            </div>
           </header>
           {/* MOB: Search Bar */}
           <SearchBar styles='mt-6 lg:hidden flex max-w-full' />
         </div>
         {/* Content */}
-        <section className='flex flex-wrap gap-4 border lg:px-[14.6rem] w-full'>
+        <section className='flex flex-wrap gap-4 lg:px-[14.6rem] w-full'>
           {/* Left */}
           <div className='border w-full lg:max-w-3xl max-w-full space-y-8'>
             {/* Contents */}
@@ -343,7 +366,7 @@ const Draft = () => {
             </div>
           </div>
           {/* Right */}
-          <div className='flex-1 border'>
+          <div className='flex-1'>
             <h1 className='text-neutral-300 text-3xl'>"Hello, World!" program</h1>
             <h3 className='text-neutral-400 text-md flex items-center gap-2'>
               Computer program
@@ -353,14 +376,14 @@ const Draft = () => {
             </h3>
             {/*  */}
             <div className='grid grid-cols-2 grid-rows-2 mt-4 w-full h-48'>
-              <div className='border-2 row-span-full border-neutral-300'>
-                <img src="https://placehold.co/40x40" alt="Sample Image" className='object-cover w-full h-full' />
+              <div className='border-2 row-span-full border-neutral-500 rounded-l-lg'>
+                <img src="https://i.ytimg.com/vi/yePpTX5_tg0/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLBlU3TYD9rh65PKe5AjMFys23AFwQ" alt="Sample Image" className='object-cover w-full h-full rounded-l-lg' />
               </div>
-              <div className='border-2 border-neutral-300 h-full w-full'>
-                <img src="https://placehold.co/40x40" alt="Sample Image" className='object-cover w-full h-full' />
+              <div className='border-2 border-neutral-500 h-full w-full rounded-tr-lg'>
+                <img src="https://miro.medium.com/v2/resize:fit:1000/1*aPfdrf5Y14OzFUXgEwB9TA.jpeg" alt="Sample Image" className='object-cover w-full h-full rounded-tr-lg' />
               </div>
-              <div className='border-2 border-neutral-300 h-full w-full'>
-                <img src="https://placehold.co/40x40" alt="Sample Image" className='object-cover w-full h-full' />
+              <div className='border-2 border-neutral-500 h-full w-full rounded-br-lg'>
+                <img src="https://mathspp.com/blog/the-most-obscure-hello-world-program/thumbnail.webp" alt="Sample Image" className='object-cover w-full h-full rounded-br-lg' />
               </div>
             </div>
             {/*  */}
